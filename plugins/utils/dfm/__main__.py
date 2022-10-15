65133b00d48ce0640daa7eaa3365d8f8e11f7d35
jsjsjsjsjsjsjsjsjsjsjsjs <108718206+jsjsjsjsjsjsjsjsjsjsjsjs@users.noreply.github.com>
2022-10-15 08:49:39 +0700

    Update __main__.py

--- plugins/utils/dfm/__main__.py
+++ plugins/utils/dfm/__main__.py
@@ -15,101 +15,56 @@ header = {"AUTH_KEY":"meki"}
     "ssh_do", about={
         'header': "Create SSH Account",
         'description': "kegabutan yg haqiqi",
-        'usage': "{tr}ssh_do [user]:[pw]:[exp]"})
-async def ssh(msg: Message):
-    """SSH account"""
-    replied = msg.input_str 
+        'usage': "{tr}ssh_do [user]:[exp]:[pw]"})
+async def _sshh(message: Message):
+    replied = message.input_str
     if not replied:
-        await msg.err("```Isi user:exp blok....```", del_in=5)
+        await message.err("```Isi user:pw:exp blok....```", del_in=5)
         return
     if ":" not in replied:
-        await msg.err("```Format harus user:pw:exp...```", del_in=5) 
+        await message.err("```Format harus user:pw:exp...```", del_in=5) 
         return
-    await msg.edit("```Sedang membuat akun, tunggu...```")
-    async with aiohttp.ClientSession() as req:
-        u = replied.strip().split(':')[0]
-        p = replied.strip().split(':')[1]
-        e = replied.strip().split(':')[2]
-        domain1 = f"188.166.177.182"
-        domain = f"d2ee8l6yhdug7c.cloudfront.net"
-        param = f":6969/adduser/exp??user={u}&password={p}&exp={e}"
-        url = ("http://"+domain1+param)
-        async with req.get(url, headers=header) as resp:
-            if resp.status != "success":
-            #return 
-              today = DT.date.today()
-              later = today + DT.timedelta(days=int(e))
-              await msg.edit(
-              text=(f"**━━━━━━━━━━━━━━━━**\n"
+    if ":"":" not in replied:
+        await message.err("```Format harus user:pw:exp...```", del_in=5) 
+        return
+    await message.edit("```Sedang membuat akun, tunggu...```")
+    u = message.input_str.strip().split(':')[0]
+    p = message.input_str.strip().split(':')[1]
+    e = message.input_str.strip().split(':')[2]
+    url = (
+        f"http://188.166.177.182:6969/adduser/exp?user={u}&password={p}&exp={e}"
+    )
+    async with aiohttp.ClientSession() as requests:
+        data = await requests.get(url, headers=header)
+        x = await data.text()
+    #status = x['status']
+    if x != "success":
+        await message.edit("Kebanyakn coli ente...!`")
+        return
+    today = DT.date.today()
+    later = today + DT.timedelta(days=int(e))
+    await message.edit(
+        text=(f"**━━━━━━━━━━━━━━━━**\n"
               f"** ⟨ SSH Account ⟩** \n"
               f"**━━━━━━━━━━━━━━━━**\n"
               f"**Username:** `{u}`\n"
               f"**Password:** `{p}`\n"
-              f"**Domain:** `{domain}`\n"
-              f"**Port SSL :** `222, 447`\n"
-              f"**Port WS :** `80`\n"
+              f"**Domain:** `rkr0.autocf.site`\n"
+              f"**DNS Domain:** `rkr0nsque.keongdns.my.id`\n"
+              f"**Port SSL :** `222, 777`\n"
+              f"**Port WS :** `2082`\n"
               f"**Port WS SSL :** `443`\n"
               f"**━━━━━━━━━━━━━━━━**\n"
               f"**PayLoad WS:**\n"
-              f"**`GET / HTTP/1.1[crlf]Host: {domain}[crlf]Connection: Keep-Alive[crlf]User-Agent: [ua][crlf]Upgrade: websocket[crlf][crlf]`\n"
+              f"**`GET / HTTP/1.1[crlf]Host: rkr0.autocf.site[crlf]Connection: Keep-Alive[crlf]User-Agent: [ua][crlf]Upgrade: websocket[crlf][crlf]`\n"
+              f"**━━━━━━━━━━━━━━━━**\n"
+              f"**Pub Key Slowlkeong:**\n"
+              f"**`3016fc65960efef9d6c35aca680c10e478ad31b406c9a47eed44d955dcac2f27`\n"
               f"**━━━━━━━━━━━━━━━━**\n"
               f"**Exp Until:** `{later}`\n"
               f"**━━━━━━━━━━━━━━━━**\n"),
          
-             disable_web_page_preview=True,
-             parse_mode=enums.ParseMode.MARKDOWN
+        disable_web_page_preview=True,
+        parse_mode=enums.ParseMode.MARKDOWN
      
-    
-        )
-        
-@userge.on_cmd(
-    "ssh_lin2", about={
-        'header': "Create SSH Account",
-        'description': "kegabutan yg haqiqi",
-        'usage': "{tr}ssh_lin2 [user]:[pw]:[exp]"})
-async def ssh(msg: Message):
-    """SSH account"""
-    replied = msg.input_str 
-    if not replied:
-        await msg.err("```Isi user:exp blok....```", del_in=5)
-        return
-    if ":" not in replied:
-        await msg.err("```Format harus user:pw:exp...```", del_in=5) 
-        return
-    await msg.edit("```Sedang membuat akun, tunggu...```")
-    async with aiohttp.ClientSession() as req:
-        u = replied.strip().split(':')[0]
-        p = replied.strip().split(':')[1]
-        e = replied.strip().split(':')[2]
-        domain1 = f"192.53.117.74"
-        domain = f"d8wtmffs74km9.cloudfront.net"
-        param = f":6969/adduser/exp??user={u}&password={p}&exp={e}"
-        url = ("http://"+domain1+param)
-        async with req.get(url, headers=header) as resp:
-            if resp.status != "success":
-            #return 
-              today = DT.date.today()
-              later = today + DT.timedelta(days=int(e))
-              await msg.edit(
-              text=(f"**━━━━━━━━━━━━━━━━**\n"
-              f"** ⟨ SSH Account ⟩** \n"
-              f"**━━━━━━━━━━━━━━━━**\n"
-              f"**Username:** `{u}`\n"
-              f"**Password:** `{p}`\n"
-              f"**Domain:** `{domain}`\n"
-              f"**Port SSL :** `222, 447`\n"
-              f"**Port WS :** `80`\n"
-              f"**Port WS SSL :** `443`\n"
-              f"**━━━━━━━━━━━━━━━━**\n"
-              f"**PayLoad WS:**\n"
-              f"**`GET / HTTP/1.1[crlf]Host: {domain}[crlf]Connection: Keep-Alive[crlf]User-Agent: [ua][crlf]Upgrade: websocket[crlf][crlf]`\n"
-              f"**━━━━━━━━━━━━━━━━**\n"
-              f"**Exp Until:** `{later}`\n"
-              f"**━━━━━━━━━━━━━━━━**\n"),
-         
-             disable_web_page_preview=True,
-             parse_mode=enums.ParseMode.MARKDOWN
-     
-    
-        )
-        
+    )
