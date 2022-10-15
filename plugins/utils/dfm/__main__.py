d240bb8515b689c4e2e4edf6f01cba2f9b25090f
jsjsjsjsjsjsjsjsjsjsjsjs <108718206+jsjsjsjsjsjsjsjsjsjsjsjs@users.noreply.github.com>
2022-10-15 08:59:26 +0700

    Update __main__.py

--- plugins/utils/dfm/__main__.py
+++ plugins/utils/dfm/__main__.py
@@ -47,17 +47,65 @@ async def _sshh(message: Message):
               f"**━━━━━━━━━━━━━━━━**\n"
               f"**Username:** `{u}`\n"
               f"**Password:** `{p}`\n"
-              f"**Domain:** `rkr0.autocf.site`\n"
-              f"**DNS Domain:** `rkr0nsque.keongdns.my.id`\n"
+              f"**Domain:** `d2ee8l6yhdug7c.cloudfront.net`\n"
               f"**Port SSL :** `222, 777`\n"
-              f"**Port WS :** `2082`\n"
+              f"**Port WS :** `80`\n"
               f"**Port WS SSL :** `443`\n"
               f"**━━━━━━━━━━━━━━━━**\n"
               f"**PayLoad WS:**\n"
-              f"**`GET / HTTP/1.1[crlf]Host: rkr0.autocf.site[crlf]Connection: Keep-Alive[crlf]User-Agent: [ua][crlf]Upgrade: websocket[crlf][crlf]`\n"
+              f"**`GET / HTTP/1.1[crlf]Host: d2ee8l6yhdug7c.cloudfront.net[crlf]Connection: Keep-Alive[crlf]User-Agent: [ua][crlf]Upgrade: websocket[crlf][crlf]`\n"
               f"**━━━━━━━━━━━━━━━━**\n"
-              f"**Pub Key Slowlkeong:**\n"
-              f"**`3016fc65960efef9d6c35aca680c10e478ad31b406c9a47eed44d955dcac2f27`\n"
+              f"**Exp Until:** `{later}`\n"
+              f"**━━━━━━━━━━━━━━━━**\n"),
+         
+        disable_web_page_preview=True,
+        parse_mode=enums.ParseMode.MARKDOWN
+     
+    )
+
+@userge.on_cmd(
+    "ssh_do", about={
+        'header': "Create SSH Account",
+        'description': "kegabutan yg haqiqi",
+        'usage': "{tr}ssh_do [user]:[exp]:[pw]"})
+async def _sshh(message: Message):
+    replied = message.input_str
+    if not replied:
+        await message.err("```Isi user:pw:exp blok....```", del_in=5)
+        return
+    if ":" not in replied:
+        await message.err("```Format harus user:pw:exp...```", del_in=5) 
+        return
+    
+    await message.edit("```Sedang membuat akun, tunggu...```")
+    u = message.input_str.strip().split(':')[0]
+    p = message.input_str.strip().split(':')[1]
+    e = message.input_str.strip().split(':')[2]
+    url = (
+        f"http://192.53.117.74:6969/adduser/exp?user={u}&password={p}&exp={e}"
+    )
+    async with aiohttp.ClientSession() as requests:
+        data = await requests.get(url, headers=header)
+        x = await data.text()
+    #status = x['status']
+    if x != "success":
+        await message.edit("Kebanyakn coli ente...!`")
+        return
+    today = DT.date.today()
+    later = today + DT.timedelta(days=int(e))
+    await message.edit(
+        text=(f"**━━━━━━━━━━━━━━━━**\n"
+              f"** ⟨ SSH Account ⟩** \n"
+              f"**━━━━━━━━━━━━━━━━**\n"
+              f"**Username:** `{u}`\n"
+              f"**Password:** `{p}`\n"
+              f"**Domain:** `d8wtmffs74km9.cloudfront.net`\n"
+              f"**Port SSL :** `222, 777`\n"
+              f"**Port WS :** `80`\n"
+              f"**Port WS SSL :** `443`\n"
+              f"**━━━━━━━━━━━━━━━━**\n"
+              f"**PayLoad WS:**\n"
+              f"**`GET / HTTP/1.1[crlf]Host: d8wtmffs74km9.cloudfront.net[crlf]Connection: Keep-Alive[crlf]User-Agent: [ua][crlf]Upgrade: websocket[crlf][crlf]`\n"
               f"**━━━━━━━━━━━━━━━━**\n"
               f"**Exp Until:** `{later}`\n"
               f"**━━━━━━━━━━━━━━━━**\n"),
