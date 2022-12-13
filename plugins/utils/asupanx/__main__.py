6dea7bbe9b7a468fdaebd26297a4da030d211a7b
jsjsjsjsjsjsjsjsjsjsjsjs <108718206+jsjsjsjsjsjsjsjsjsjsjsjs@users.noreply.github.com>
2022-12-13 18:49:17 +0700

    Create __main__.py

---
+++ plugins/utils/asupanx/__main__.py
@@ -0,0 +1,31 @@
+from asyncio import gather
+from random import choice
+
+from pyrogram import Client, enums, filters
+from pyrogram.types import Message
+from userge import userge
+from userge import edit_or_reply
+from userge import ReplyCheck
+
+@userge.on_cmd(
+    "asupanx", about={
+        'header': "Kegoblokan yang haqiqi",
+        'description': "Kegoblokan yg haqiqi",
+        'usage': "{tr}asupanx"})
+async def asupanx(client: Client, message: Message):
+	Asu = await edit_or_reply(message, "`Tunggu Sebentar...`")
+    await gather(
+        Asu.delete(),
+        client.send_video(
+            message.chat.id,
+            choice(
+                [
+                    asupanx.video.file_id
+                    async for asupanx in client.search_messages(
+                        "tedeasupancache", filter=enums.MessagesFilter.VIDEO
+                    )
+                ]
+            ),
+            reply_to_message_id=ReplyCheck(message),
+        ),
+    )
