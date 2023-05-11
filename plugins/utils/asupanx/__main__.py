529abba25f6600518d35c94de94a2accff9efe54
jsjsjsjsjsjsjsjsjsjsjsjs <108718206+jsjsjsjsjsjsjsjsjsjsjsjs@users.noreply.github.com>
2023-05-11 17:09:30 +0700

    Update __main__.py

--- plugins/utils/asupanx/__main__.py
+++ plugins/utils/asupanx/__main__.py
@@ -1,3 +1,4 @@
+from asyncio import *
 import aiohttp
 import base64
 from pyrogram import enums
@@ -11,19 +12,22 @@ from userge import userge, Message, filters
 
 
 @userge.on_cmd("asupan", about="asupan")
-async def asupan_cmd(client: Client, message: Message):
-    m = await message.reply(message, "`Tunggu Sebentar...`")
+async def asupan(client: Client, message: Message):
+    if message.chat.id == -1001554560763:
+        return await edit_or_reply(message, "**This command is prohibited from being used in this group**")
+    ram = await edit_or_reply(message, "`Wait a moment...`")
     await gather(
+        ram.delete(),
         client.send_video(
             message.chat.id,
             choice(
                 [
                     asupan.video.file_id
                     async for asupan in client.search_messages(
-                        "tedeasupancache", filter="video"
+                        "punyakenkan", filter=enums.MessagesFilter.VIDEO
                     )
                 ]
             ),
+            reply_to_message_id=ReplyCheck(message),
         ),
-        m.delete(),
     )
