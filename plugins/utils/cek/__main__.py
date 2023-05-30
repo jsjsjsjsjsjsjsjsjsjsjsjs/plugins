0165d8b02b2d61ff74440ace04bb523c7447423d
jsjsjsjsjsjsjsjsjsjsjsjs <108718206+jsjsjsjsjsjsjsjsjsjsjsjs@users.noreply.github.com>
2023-05-30 22:36:58 +0700

    Create __main__.py

---
+++ plugins/utils/cek/__main__.py
@@ -0,0 +1,17 @@
+from tcping import Ping
+
+
+@userge.on_cmd("cek", about="cek server ping")
+async def ping(message: Message):
+    replied = message.input_str 
+    if not replied:
+        await message.edit("`JGN KOSONG BLOK!`")
+        return
+    await message.edit("`Tunggu Blog !`")
+    s = replied.strip().split(':')[0]
+    z = ping.ping(3)
+    if len(list(z)) == 3:
+         await message.edit(f" `{z}\n`")
+    else:
+          await message.edit(f" `Not Responds`")
+     
